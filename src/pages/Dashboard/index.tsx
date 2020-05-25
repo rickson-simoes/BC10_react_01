f3e16e9697c7fda9f50098d272226854e2563964
Rickson <rickson.simoes@hotmail.com>
2020-05-25 20:29:32 -0300

    :art: Estilizacao do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import LogoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  return (
+    <>
+      <img src={LogoImg} alt="Github Logo" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="ae">
+          <img
+            src="https://avatars1.githubusercontent.com/u/37545464?s=460&u=89d123fda0fcc7f9a390a0c1015f3c002ecb181d&v=4"
+            alt="teste"
+          />
+          <div>
+            <strong> Rickson unform </strong>
+            <p>Mais um projeto de listas de repositórios do github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
