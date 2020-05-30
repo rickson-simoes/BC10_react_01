02c0655d8f26d864a682055a59f22a69d0319c7b
Rickson <rickson.simoes@hotmail.com>
2020-05-30 12:38:39 -0300

    Navegando entre rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import LogoImg from '../../assets/logo.svg';
@@ -73,7 +74,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repos.map((repository) => (
-          <a key={repository.full_name} href="ae">
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -84,7 +88,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
